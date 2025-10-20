fdf9ed8d0aa6102847fb28cceae2aa9eaf246fc7
ennio <e.pagano@alumno.um.edu.ar>
2025-10-20 01:55:35 +0000

    Implementada función tirar

--- core/src/dados.py
+++ core/src/dados.py
@@ -1,4 +1,6 @@
-class Dados:
+import random
+
+class Dice:
     """Clase que maneja los dados
     
     Atributos
@@ -9,3 +11,31 @@ class Dados:
 
     def __init__(self):
         self.__valores__ = []
+
+    def tirar(self) -> List[]:
+        """Tira dos dados de 6 caras, si son iguales, se pueden usar 4 veces
+
+         Retorna
+        -------
+        List[int]
+            Una copia de los valores de los dados (2 o 4 elementos si son dobles)
+        """
+
+        dado1 = random.randint(1, 6)
+        dado2 = random.randint(1, 6)
+
+        if dado1 == dado2:
+            self.__valores__ == [dado1, dado1, dado1, dado1]
+        else:
+            self.__valores__ == [dado1, dado2]
+
+        return self.__valores__.copy()
+
+    @property
+    def valores(self) -> List:
+        """Devuelve una copia de los valores actuales"""
+        return self.__valores__.copy()
+
+    
+
+    
